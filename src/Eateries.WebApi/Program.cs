426d46d901fd97e80ca60ec3779a8418d97db0b7
Artash Ter-Abramian <artash.terabrahamyan@mail.ru>
2023-01-17 21:57:36 +0400

    Program.cs file Extensions added

--- src/Eateries.WebApi/Program.cs
+++ src/Eateries.WebApi/Program.cs
@@ -1,6 +1,9 @@
+using Eateries.Application;
 using Eateries.Infrastructure.Persistence;
+using Eateries.Infrastructure.Persistence.Contexts;
 using Eateries.Infrastructure.Shared;
 using Eateries.WebApi.Extensions;
+using Microsoft.EntityFrameworkCore;
 using Serilog;
 
 try
@@ -13,13 +16,19 @@ try
     .Enrich.FromLogContext()
     .CreateLogger();
     builder.Host.UseSerilog(Log.Logger);
+    builder.Services.AddApplicationLayer();
     builder.Services.AddPersistenceInfrastructure(builder.Configuration);
     builder.Services.AddSharedInfrastructure(builder.Configuration);
-    builder.Services.AddSwaggerGen();
+    builder.Services.AddSwaggerExtension();
     builder.Services.AddControllerExtension();
     // CORS
     builder.Services.AddCorsExtension();
     builder.Services.AddHealthChecks();
+    //API Security
+    builder.Services.AddJWTAuthentication(builder.Configuration);
+    //builder.Services.AddAuthorizationPolicies(builder.Configuration);
+    // API version
+    //builder.Services.AddApiVersioningExtension();
     // API explorer
     builder.Services.AddMvcCore()
                     .AddApiExplorer();
@@ -29,18 +38,39 @@ try
     var app = builder.Build();
 
     // Configure the HTTP request pipeline.
+
     if (app.Environment.IsDevelopment())
     {
+        app.UseDeveloperExceptionPage();
+
         app.UseSwagger();
         app.UseSwaggerUI();
     }
+    else
+    {
+        app.UseExceptionHandler("/Error");
+        app.UseHsts();
+    }
 
+    using (var scope = app.Services.CreateScope())
+    {
+        var dbContext = scope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
+        // use context
+        await dbContext.Database.MigrateAsync();
+    }
+
+    // Add this line; you'll need `using Serilog;` up the top, too
+    app.UseSerilogRequestLogging();
     app.UseHttpsRedirection();
-
+    app.UseRouting();
+    //Enable CORS
+    app.UseCors("AllowAll");
+    app.UseAuthentication();
     app.UseAuthorization();
-
+    app.UseSwaggerExtension();
+    app.UseErrorHandlingMiddleware();
+    app.UseHealthChecks("/health");
     app.MapControllers();
-
     app.Run();
 }
 catch (Exception ex)
