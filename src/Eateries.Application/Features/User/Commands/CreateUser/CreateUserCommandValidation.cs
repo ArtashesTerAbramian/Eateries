860d00e5376cddf4e4d94427754459ec32204d71
Artashes Ter-Abrahamyan <60017912+ArtashesTerAbramian@users.noreply.github.com>
2023-02-19 19:08:54 +0400

    User entity validation

--- src/Eateries.Application/Features/User/Commands/CreateUser/CreateUserCommandValidation.cs
+++ src/Eateries.Application/Features/User/Commands/CreateUser/CreateUserCommandValidation.cs
@@ -11,13 +11,19 @@ public class CreateUserCommandValidation : AbstractValidator<CreateUserCommand>
     {
         _userRepositoryAsync = userRepositoryAsync;
 
-        RuleFor(r => r.FirstName)
-            .NotEmpty().WithMessage("{PropertyName} can't be empty");
-        RuleFor(r => r.LastName)
-            .NotEmpty().WithMessage("{PropertyName} can't be empty");
-        RuleFor(r => r.Email)
-            .NotEmpty().WithMessage("{PropertyName} can't be empty");
-        RuleFor(r => r.Password)
-            .NotEmpty().WithMessage("{PropertyName} can't be empty");
+
+        RuleFor(x => x.FirstName)
+            .NotEmpty()
+            .MaximumLength(50);
+        RuleFor(x => x.LastName)
+            .NotEmpty()
+            .MaximumLength(50);
+        RuleFor(x => x.Email)
+            .NotEmpty()
+            .EmailAddress();
+        RuleFor(x => x.Password)
+            .NotEmpty()
+            .MinimumLength(8);
+
     }
 }
