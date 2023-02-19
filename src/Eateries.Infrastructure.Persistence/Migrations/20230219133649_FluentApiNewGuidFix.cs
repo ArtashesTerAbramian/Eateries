4a9a0a3991bf9011214275fc243678aa3e77b0a4
Artashes Ter-Abrahamyan <60017912+ArtashesTerAbramian@users.noreply.github.com>
2023-02-19 19:09:22 +0400

    Migrations

---
+++ src/Eateries.Infrastructure.Persistence/Migrations/20230219133649_FluentApiNewGuidFix.cs
@@ -0,0 +1,145 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Eateries.Infrastructure.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class FluentApiNewGuidFix : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<Guid>(
+                name: "Id",
+                table: "Users",
+                type: "uniqueidentifier",
+                nullable: false,
+                defaultValueSql: "NEWID()",
+                oldClrType: typeof(Guid),
+                oldType: "uniqueidentifier");
+
+            migrationBuilder.AlterColumn<Guid>(
+                name: "Id",
+                table: "Menus",
+                type: "uniqueidentifier",
+                nullable: false,
+                defaultValueSql: "NEWID()",
+                oldClrType: typeof(Guid),
+                oldType: "uniqueidentifier");
+
+            migrationBuilder.AlterColumn<Guid>(
+                name: "Id",
+                table: "Ingredients",
+                type: "uniqueidentifier",
+                nullable: false,
+                defaultValueSql: "NEWID()",
+                oldClrType: typeof(Guid),
+                oldType: "uniqueidentifier");
+
+            migrationBuilder.AlterColumn<Guid>(
+                name: "Id",
+                table: "Eateries",
+                type: "uniqueidentifier",
+                nullable: false,
+                defaultValueSql: "NEWID()",
+                oldClrType: typeof(Guid),
+                oldType: "uniqueidentifier");
+
+            migrationBuilder.AlterColumn<Guid>(
+                name: "Id",
+                table: "Dishes",
+                type: "uniqueidentifier",
+                nullable: false,
+                defaultValueSql: "NEWID()",
+                oldClrType: typeof(Guid),
+                oldType: "uniqueidentifier");
+
+            migrationBuilder.AlterColumn<Guid>(
+                name: "Id",
+                table: "Cuisines",
+                type: "uniqueidentifier",
+                nullable: false,
+                defaultValueSql: "NEWID()",
+                oldClrType: typeof(Guid),
+                oldType: "uniqueidentifier");
+
+            migrationBuilder.AlterColumn<Guid>(
+                name: "Id",
+                table: "Addresses",
+                type: "uniqueidentifier",
+                nullable: false,
+                defaultValueSql: "NEWID()",
+                oldClrType: typeof(Guid),
+                oldType: "uniqueidentifier");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<Guid>(
+                name: "Id",
+                table: "Users",
+                type: "uniqueidentifier",
+                nullable: false,
+                oldClrType: typeof(Guid),
+                oldType: "uniqueidentifier",
+                oldDefaultValueSql: "NEWID()");
+
+            migrationBuilder.AlterColumn<Guid>(
+                name: "Id",
+                table: "Menus",
+                type: "uniqueidentifier",
+                nullable: false,
+                oldClrType: typeof(Guid),
+                oldType: "uniqueidentifier",
+                oldDefaultValueSql: "NEWID()");
+
+            migrationBuilder.AlterColumn<Guid>(
+                name: "Id",
+                table: "Ingredients",
+                type: "uniqueidentifier",
+                nullable: false,
+                oldClrType: typeof(Guid),
+                oldType: "uniqueidentifier",
+                oldDefaultValueSql: "NEWID()");
+
+            migrationBuilder.AlterColumn<Guid>(
+                name: "Id",
+                table: "Eateries",
+                type: "uniqueidentifier",
+                nullable: false,
+                oldClrType: typeof(Guid),
+                oldType: "uniqueidentifier",
+                oldDefaultValueSql: "NEWID()");
+
+            migrationBuilder.AlterColumn<Guid>(
+                name: "Id",
+                table: "Dishes",
+                type: "uniqueidentifier",
+                nullable: false,
+                oldClrType: typeof(Guid),
+                oldType: "uniqueidentifier",
+                oldDefaultValueSql: "NEWID()");
+
+            migrationBuilder.AlterColumn<Guid>(
+                name: "Id",
+                table: "Cuisines",
+                type: "uniqueidentifier",
+                nullable: false,
+                oldClrType: typeof(Guid),
+                oldType: "uniqueidentifier",
+                oldDefaultValueSql: "NEWID()");
+
+            migrationBuilder.AlterColumn<Guid>(
+                name: "Id",
+                table: "Addresses",
+                type: "uniqueidentifier",
+                nullable: false,
+                oldClrType: typeof(Guid),
+                oldType: "uniqueidentifier",
+                oldDefaultValueSql: "NEWID()");
+        }
+    }
+}
