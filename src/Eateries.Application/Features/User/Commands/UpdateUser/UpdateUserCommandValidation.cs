7e1b1e954a7700cd4239da3068cffc9411e41840
Artashes Ter-Abrahamyan <60017912+ArtashesTerAbramian@users.noreply.github.com>
2023-02-25 23:35:12 +0400

    UpdateUserCommandValidation.cs fix

--- src/Eateries.Application/Features/User/Commands/UpdateUser/UpdateUserCommandValidation.cs
+++ src/Eateries.Application/Features/User/Commands/UpdateUser/UpdateUserCommandValidation.cs
@@ -14,16 +14,12 @@ public class UpdateUserCommandValidation : AbstractValidator<UpdateUserCommand>
 
 
         RuleFor(x => x.FirstName)
-            .NotEmpty()
             .MaximumLength(50);
         RuleFor(x => x.LastName)
-            .NotEmpty()
             .MaximumLength(50);
         RuleFor(x => x.Email)
-            .NotEmpty()
             .EmailAddress();
         RuleFor(x => x.Password)
-            .NotEmpty()
             .MinimumLength(8);
 
     }
